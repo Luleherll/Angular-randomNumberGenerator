44ad3161bfca9745435cb239bd18f38e5f1f8267
Luleherll <luleherll@gmail.com>
2019-08-13 11:51:07 +0300

    story(numbers): generate numbers
    - implement logic for generating numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material';
 
@@ -7,11 +7,41 @@ import { MatIconRegistry } from '@angular/material';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
     this.matIconRegistry.addSvgIcon('down', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/caret-down.svg'));
     this.matIconRegistry.addSvgIcon('up', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/sort-up.svg'));
   }
   title = 'randomNumberGenerator';
-  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  numberList: Set<any>;
+  times = [];
+
+  ngOnInit(): void {
+    this.numberList = new Set();
+  }
+
+  newNumber = (code: string = '075') => {
+    const random = parseInt(((Math.random() * 9 + 1) * Math.pow(10, 6)).toString(), 10);
+    return code + random.toString();
+  }
+
+  generateNumbers = (total: number) => {
+    const iterations = total / 100;
+    this.times = new Array(iterations).fill(100);
+    while (this.numberList.size < total) {
+      this.times.forEach(number => {
+      const i = this.runLoop(number);
+      i.forEach(value => this.numberList.add(value));
+    });
+    }
+  }
+
+  runLoop = (number) => {
+      const arr = new Set();
+      for (let i = 0; i < number; i++) {
+      arr.add(this.newNumber());
+    }
+      return arr;
+    }
+
 }
